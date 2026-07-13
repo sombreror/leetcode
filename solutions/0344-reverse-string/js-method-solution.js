c220248160487d2b5fc1f572beaa0b85282c707b
sombreror <somber974@gmail.com>
2026-07-13 10:27:42 +0200

    fix: 0344 missing module

--- solutions/0344-reverse-string/js-method-solution.js
+++ solutions/0344-reverse-string/js-method-solution.js
@@ -4,7 +4,11 @@ class Solution {
      * @return {void} Do not return anything, modify s in-place instead.
      */
     //utilize the method to reverse of an array to reverse the array as the solution says.
+    //we should have used the two pointers solution, even tough the time and space complexity for this solution is too: O(n) and O(1);
+    //its the same as the two pointers solution.
     reverseString(s) {
         s.reverse();
     }
 }
+
+module.exports = Solution;
--- solutions/0344-reverse-string/solution.js
+++ solutions/0344-reverse-string/solution.js
@@ -20,3 +20,5 @@ class Solution {
         }
     }
 }
+
+module.exports = Solution;
