249dbd97e339f57a15054bb239bed73273001310
sombreror <somber974@gmail.com>
2026-07-14 16:37:12 +0200

    release solution => 0912: js method solution + merge sort

---
+++ solutions/0912-sort-an-array/js-method-solution.js
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums) {
+        return nums.sort((a, b) => a - b);
+    }
+}
+
+module.exports = Solution;
